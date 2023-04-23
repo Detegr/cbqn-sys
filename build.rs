44c9a28875ac41c651c325c0dd758e8f70b1d793
Antti Keränen <detegr@rbx.email>
2023-04-23 21:37:51 +0300

    Update to CBQN release 0.1.0
    Version numbers will follow CBQN's versioning from now on.
    
    - Add support for building without bindgen using pregenerated bindings.
    - Add support for not linking to libcbqn.so for WASI target

--- build.rs
+++ build.rs
@@ -2,59 +2,96 @@
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
 
+use std::{env, path::PathBuf};
+
+#[cfg(feature = "bindgen")]
 use bindgen;
+
+#[cfg(feature = "shared-object")]
+use std::{fs, process::Command};
+
+#[cfg(feature = "shared-object")]
 use fs_extra::dir;
-use std::{env, fs, path::PathBuf, process::Command};
 
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let cbqn_dir = out_dir.join("CBQN");
-    let bytecode_dir = cbqn_dir.join("build/bytecodeLocal");
+    #[cfg(feature = "shared-object")]
+    {
+        let cbqn_dir = out_dir.join("CBQN");
+        let bytecode_dir = cbqn_dir.join("build/bytecodeLocal");
 
-    dir::remove(&cbqn_dir).unwrap();
+        dir::remove(&cbqn_dir).unwrap();
 
-    // build.rs must not modify anything outside $OUT_DIR, so copying CBQN to there
-    dir::copy("CBQN", &out_dir, &dir::CopyOptions::new())
-        .expect("expected to copy CBQN to $OUT_DIR/CBQN");
+        // build.rs must not modify anything outside $OUT_DIR, so copying CBQN to there
+        dir::copy("CBQN", &out_dir, &dir::CopyOptions::new())
+            .expect("expected to copy CBQN to $OUT_DIR/CBQN");
 
-    fs::create_dir(&bytecode_dir).expect("expected to create build/bytecodeLocal");
-    dir::copy(
-        cbqn_dir.join("build/bytecodeSubmodule/gen"),
-        &bytecode_dir,
-        &dir::CopyOptions::new(),
-    )
-    .expect("expected to copy prebuilt bytecode to local bytecode to avoid using git during build");
+        fs::create_dir(&bytecode_dir).expect("expected to create build/bytecodeLocal");
+        dir::copy(
+            cbqn_dir.join("build/bytecodeSubmodule/gen"),
+            &bytecode_dir,
+            &dir::CopyOptions::new(),
+        )
+        .expect(
+            "expected to copy prebuilt bytecode to local bytecode to avoid using git during build",
+        );
 
-    let cbqn_build = Command::new("make")
-        .arg("shared-o3")
-        .arg("libcbqn.so")
-        .current_dir(&cbqn_dir)
-        .status()
-        .expect("make");
+        let clean_first = Command::new("make")
+            .arg("clean")
+            .current_dir(&cbqn_dir)
+            .status()
+            .expect("make clean");
 
-    if !cbqn_build.success() {
-        std::process::exit(1);
+        if !clean_first.success() {
+            std::process::exit(1);
+        }
+
+        let for_build = Command::new("make")
+            .arg("for-build")
+            .current_dir(&cbqn_dir)
+            .status()
+            .expect("make for-build");
+
+        if !for_build.success() {
+            std::process::exit(1);
+        }
+
+        let cbqn_build = Command::new("make")
+            .arg("shared-o3")
+            .arg("libcbqn.so")
+            .current_dir(&cbqn_dir)
+            .status()
+            .expect("make");
+
+        if !cbqn_build.success() {
+            std::process::exit(1);
+        }
+
+        println!("cargo:rustc-link-search=native={}", cbqn_dir.display());
+        println!("cargo:rustc-link-lib=cbqn");
+    };
+
+    #[cfg(feature = "bindgen")]
+    {
+        let bindings = bindgen::Builder::default()
+            // The input header we would like to generate
+            // bindings for.
+            .header("CBQN/include/bqnffi.h")
+            .allowlist_function("bqn_.*")
+            // Tell cargo to invalidate the built crate whenever any of the
+            // included header files changed.
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            // Finish the builder and generate the bindings.
+            .generate()
+            // Unwrap the Result and panic on failure.
+            .expect("Unable to generate bindings");
+
+        bindings
+            .write_to_file(out_dir.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+
+        println!("cargo:rerun-if-changed=CBQN/include/bqnffi.h");
     }
 
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header("CBQN/include/bqnffi.h")
-        .allowlist_function("bqn_.*")
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    bindings
-        .write_to_file(out_dir.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-
-    println!("cargo:rustc-link-search=native={}", cbqn_dir.display());
-    println!("cargo:rustc-link-lib=cbqn");
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=CBQN/include/bqnffi.h");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,14 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(not(feature = "bindgen"))]
+mod pregenerated;
+
+#[cfg(not(feature = "bindgen"))]
+pub use pregenerated::*;
 
 #[cfg(test)]
 mod test {
