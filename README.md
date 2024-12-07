94a08085de5ac0b41c0e7ad443cc415316b64932
Antti Keränen <detegr@rbx.email>
2024-12-07 23:57:41 +0200

    Remove bindgen feature

--- README.md
+++ README.md
@@ -8,13 +8,13 @@ A crate providing FFI bindings to [CBQN](https://github.com/dzaima/CBQN).
 
 The default feature is `shared-object` which links to libcbqn.so. It can be built using `shared-o3` target. Use `RUSTFLAGS="-L /path/to/cbqn"` and `LD_LIBRARY_PATH=/path/to/cbqn/libcbqn.so` if the shared object is not in a system-wide path.
 
-### bindgen
-
-The `bindgen` feature generates new bindings using [Bindgen](https://docs.rs/bindgen/latest/bindgen/).
-
 ### No features
 
 Building without features gives only the API to CBQN. Used with [cbqn-rs](https://github.com/Detegr/cbqn-rs) WASI backend for example.
+
+## Pregenerated bindings
+
+Pregenerated bindings can be found from `src/pregenerated.rs`. To recreate them, use bindgen-cli.
 
 ## License
 
--- build.rs
+++ build.rs
@@ -2,41 +2,12 @@
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
 
-#[cfg(feature = "bindgen")]
-use std::{env, path::PathBuf};
-
-#[cfg(feature = "bindgen")]
-use bindgen;
-
 fn main() {
-
     #[cfg(feature = "shared-object")]
     {
         println!("cargo:rustc-link-lib=cbqn");
     };
 
-    #[cfg(feature = "bindgen")]
-    {
-        let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-        let bindings = bindgen::Builder::default()
-            // The input header we would like to generate
-            // bindings for.
-            .header("CBQN/include/bqnffi.h")
-            .allowlist_function("bqn_.*")
-            // Tell cargo to invalidate the built crate whenever any of the
-            // included header files changed.
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-            // Finish the builder and generate the bindings.
-            .generate()
-            // Unwrap the Result and panic on failure.
-            .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(out_dir.join("bindings.rs"))
-            .expect("Couldn't write bindings!");
-
-        println!("cargo:rerun-if-changed=CBQN/include/bqnffi.h");
-    }
-
+    println!("cargo:rerun-if-changed=src/pregenerated.rs");
     println!("cargo:rerun-if-changed=build.rs");
 }
